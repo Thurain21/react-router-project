e10e50c123fb9a49ba1f520249f093b35c12937c
Thurain21 <thurainkosg@gmail.com>
2024-11-06 18:01:17 +0800

    modified for react-router and nav components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+import { Routes,Route } from 'react-router';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar/>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
     </div>
   );
 }
